dca2c4f2c95a640565105249d8c1bae2912441a9
Guransh <guransh1896.be21@chitkara.edu.in>
2024-07-08 17:39:43 +0530

    GSAP

---
+++ GSAP/script.js
@@ -0,0 +1,27 @@
+gsap.to(".box1",{
+    x: 100,
+    
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "blue",
+  
+
+})
+gsap.to(".box2",{
+    x: 100,
+    scale: 0.5,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "pink",
+})
+gsap.to(".box3",{
+    x: 100,
+    duration: 2,
+    delay: 1,
+    rotate: 360,
+    backgroundColor: "white",
+
+
+})
